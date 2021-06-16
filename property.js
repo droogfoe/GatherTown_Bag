ec368a9d05364813d923e6b2ed3815d29e2f2d7d
foefugl <seeddroog@gmail.com>
2021-06-16 21:24:16 +0800

    storage api testing

--- property.js
+++ property.js
@@ -1,6 +1,12 @@
 window.onload = function() { ResultFeedback() };
 
 var testString = "Hellow Word";
+
+function saveStateDataToFile()
+{
+    var blob = new Blob(["data.org"], {type: "text/plain;charset=utf-8"});
+    saveAs
+}
 function ResultFeedback()
 {
     alert(testString);
