7efb6b59f9dbbe5ee2fd2686142a0eb7674e31b3
foefugl <seeddroog@gmail.com>
2021-06-16 20:17:07 +0800

    Add result page.

---
+++ property.js
@@ -0,0 +1,11 @@
+window.onload = function() { ResultFeedback() };
+
+var testString = "Hellow Word";
+function ResultFeedback()
+{
+    alert(testString);
+}
+function StoreItem()
+{
+    testString = "You got it!";
+}
