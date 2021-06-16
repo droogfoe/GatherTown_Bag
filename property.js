227a35c87899a7ad6184df68c7b5e30359f3f073
foefugl <seeddroog@gmail.com>
2021-06-16 21:59:28 +0800

    Local storage!

--- property.js
+++ property.js
@@ -5,7 +5,6 @@ var testString = "Hellow Word";
 function saveStateDataToFile()
 {
     var blob = new Blob(["data.org"], {type: "text/plain;charset=utf-8"});
-    saveAs
 }
 function ResultFeedback()
 {
